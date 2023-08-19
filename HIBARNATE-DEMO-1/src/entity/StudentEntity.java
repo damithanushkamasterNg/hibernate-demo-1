15fbb437b100f4478efd6634a5513a895f6c8013
Damith Anushka <damithanushkam016@gmail.com>
2023-08-19 12:05:36 +0530

    Add attributes to main student table

--- HIBARNATE-DEMO-1/src/entity/StudentEntity.java
+++ HIBARNATE-DEMO-1/src/entity/StudentEntity.java
@@ -1,14 +1,32 @@
 package entity;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import entity.embeded.StudentName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="student")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class StudentEntity {
 
     @Id
@@ -18,4 +36,17 @@ public class StudentEntity {
 
     @Column(name="nic", nullable = false)
     private String nic;
+
+    //composite attribute
+    private StudentName studentName;
+
+    //multivalue attribute
+    @ElementCollection
+    @CollectionTable(name="student_moblie", joinColumns = @JoinColumn(name="student_id"))
+
+    private List<String> mobileNos;
+
+    @CreationTimestamp
+    @Column(name="created_date", nullable = false)
+    private Date createDate;
 }
