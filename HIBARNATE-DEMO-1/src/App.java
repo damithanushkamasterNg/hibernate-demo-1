8cfec0654f2753526d742a930e93ceb2f8942da5
Damith Anushka <damithanushkam016@gmail.com>
2023-08-19 12:53:53 +0530

    Add student delete / update methods

--- HIBARNATE-DEMO-1/src/App.java
+++ HIBARNATE-DEMO-1/src/App.java
@@ -1,23 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
 import entity.StudentEntity;
-import entity.embeded.StudentName;
 import repository.StudentRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         StudentRepository repository = new StudentRepository();
-        StudentEntity studentEntity = new StudentEntity();
-        StudentName studentName = new StudentName("firstName1", "lastname");
-        List<String>  moblies = new ArrayList<>();
-        moblies.add("3232323232323");
-        moblies.add("2324545646465");
+        StudentEntity studentEntity = repository.getStudent(3);
+        System.out.println(studentEntity.toString());
 
-        studentEntity.setNic("43242342V");
-        studentEntity.setStudentName(studentName);
-        studentEntity.setMobileNos(moblies);
-        repository.saveStudent(studentEntity);
+        studentEntity.setNic("881879789V");
+        studentEntity.getStudentName().setFirstName("Updated First Name");
 
+        repository.updateStudent(studentEntity);
+
+        studentEntity = repository.getStudent(3);
+        System.out.println(studentEntity.toString());
+
+        repository.deleteStudent(studentEntity);
     }
 }
--- HIBARNATE-DEMO-1/src/repository/StudentRepository.java
+++ HIBARNATE-DEMO-1/src/repository/StudentRepository.java
@@ -10,20 +10,55 @@ public class StudentRepository {
 
     Session session = SessionFactoryConfiguration.getInstance().getSession();
 
+    /** Save a student entity to the database */
     public Integer saveStudent(StudentEntity studentEntity) {
-
         Transaction transaction = session.beginTransaction();
-
+    
         try {
-
+            // Persist the studentEntity object and get the generated ID
             Integer id = (Integer) session.save(studentEntity);
-            transaction.commit();
+            transaction.commit(); // Commit the transaction
             return id;
-
+    
         } catch (Exception e) {
-            transaction.rollback();
+            transaction.rollback(); // Rollback the transaction on exception
             return -1;
         }
-
     }
+    
+    /** Retrieve a student entity by ID from the database */
+    public StudentEntity getStudent(int id) {
+        // Fetch the student entity using the session's get() method
+        StudentEntity entity = session.get(StudentEntity.class, id);
+        return entity;
+    }
+    
+    /** Update a student entity in the database */
+    public void updateStudent(StudentEntity studentEntity) {
+        Transaction transaction = session.beginTransaction();
+        try {
+            // Update the studentEntity object
+            session.update(studentEntity);
+            transaction.commit(); // Commit the transaction
+        } catch (Exception e) {
+            transaction.rollback(); // Rollback the transaction on exception
+            throw e; // Re-throw the exception
+        }
+    }
+    
+    /** Delete a student entity by studentEntity from the database */
+    public void deleteStudent(StudentEntity studentEntity) {
+        Transaction transaction = session.beginTransaction();
+        try {
+            // Delete the student entity using the session's delete() method
+            if (studentEntity != null) {
+                session.delete(studentEntity);
+            }
+            transaction.commit(); // Commit the transaction
+        } catch (Exception e) {
+            transaction.rollback(); // Rollback the transaction on exception
+            throw e; // Re-throw the exception
+        }
+    }
+    
 }
